1f1c61a19891f131aa0d4a9f4f73c85116828498
Michael Pearson <mpearson2516@yahoo.com>
2021-01-27 14:40:47 -0800

    Made some changes to my tables in my db

---
+++ App/car_dealership/src/main/java/com/dealership/model/User.java
@@ -0,0 +1,6 @@
+package com.dealership.model;
+
+public class User {
+
+	
+}
--- DB/schema.sql
+++ DB/schema.sql
@@ -2,9 +2,9 @@
 
 -- Store info about each user
 CREATE TABLE "user" (
-    "username" CHAR   NOT NULL,
-    "password" CHAR   NOT NULL,
-    "name" CHAR   NOT NULL,
+    "username" CHAR(20)   NOT NULL,
+    "password" CHAR(20)   NOT NULL,
+    "name" CHAR(20)   NOT NULL,
     "age" INT   NOT NULL,
     "dob" DATE   NOT NULL,
     CONSTRAINT "pk_user" PRIMARY KEY (
@@ -15,8 +15,8 @@ CREATE TABLE "user" (
 -- Store the info about each car and whether or not it is in the lot or not as well as if it has an owner
 CREATE TABLE "cars" (
     "car_id" INT   NOT NULL,
-    "owner" CHAR   NOT NULL,
-    "lot" CHAR   NOT NULL,
+    "owner" CHAR(20)   NOT NULL,
+    "lot" CHAR(3)   NOT NULL,
     "price" NUMERIC   NOT NULL,
     CONSTRAINT "pk_cars" PRIMARY KEY (
         "car_id"
@@ -43,7 +43,7 @@ CREATE TABLE "payment_history" (
 -- Thus this table is need so as not to break the logic in the users or cars tables.
 CREATE TABLE "customer_cars" (
     "id" INT   NOT NULL,
-    "username" CHAR   NOT NULL,
+    "username" CHAR(20)   NOT NULL,
     "car_id" INT   NOT NULL,
     CONSTRAINT "pk_customer_cars" PRIMARY KEY (
         "id"
@@ -60,3 +60,8 @@ REFERENCES "user" ("username");
 
 ALTER TABLE "customer_cars" ADD CONSTRAINT "fk_customer_cars_car_id" FOREIGN KEY("car_id")
 REFERENCES "cars" ("car_id");
+
+-- Table alterations
+-- Need to increase the size of the user name
+ALTER TABLE "user"
+ALTER COLUMN "name" TYPE CHAR(30);
