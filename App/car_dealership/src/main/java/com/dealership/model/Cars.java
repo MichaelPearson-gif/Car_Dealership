5ef9cc24c58bdf5f52772af893a6c5bda56a3197
Michael Pearson <mpearson2516@yahoo.com>
2021-01-27 17:17:06 -0800

    Added more fields to the Cars class

--- App/car_dealership/src/main/java/com/dealership/model/Cars.java
+++ App/car_dealership/src/main/java/com/dealership/model/Cars.java
@@ -5,19 +5,28 @@ public class Cars {
 	private int carId;
 	private String owner;
 	private String lot;
+	private int make;
+	private String model;
+	private String color;
 	private double price;
+	private String username;
 	
 	public Cars() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Cars(int carId, String owner, String lot, double price) {
+	public Cars(int carId, String owner, String lot, int make, String model, String color, double price,
+			String username) {
 		super();
 		this.carId = carId;
 		this.owner = owner;
 		this.lot = lot;
+		this.make = make;
+		this.model = model;
+		this.color = color;
 		this.price = price;
+		this.username = username;
 	}
 
 	public int getCarId() {
@@ -44,6 +53,30 @@ public class Cars {
 		this.lot = lot;
 	}
 
+	public int getMake() {
+		return make;
+	}
+
+	public void setMake(int make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
 	public double getPrice() {
 		return price;
 	}
@@ -52,9 +85,18 @@ public class Cars {
 		this.price = price;
 	}
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
 	@Override
 	public String toString() {
-		return "Cars [carId=" + carId + ", owner=" + owner + ", lot=" + lot + ", price=" + price + "]";
+		return "Cars [carId=" + carId + ", owner=" + owner + ", lot=" + lot + ", make=" + make + ", model=" + model
+				+ ", color=" + color + ", price=" + price + ", username=" + username + "]";
 	}
 	
 }
