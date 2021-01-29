94ee341670fa9ae88309c6d68ce7985282da3437
Michael Pearson <mpearson2516@yahoo.com>
2021-01-29 15:50:15 -0800

    Added the BusinessException and UserDAOImpl classes

---
+++ App/car_dealership/src/main/java/com/dealership/dao/impl/UserDAOImpl.java
@@ -0,0 +1,20 @@
+package com.dealership.dao.impl;
+
+import com.dealership.dao.UserDAO;
+import com.dealership.model.User;
+
+public class UserDAOImpl implements UserDAO{
+
+	@Override
+	public int createUser(User user) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String login(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ App/car_dealership/src/main/java/com/dealership/exceptions/BusinessException.java
@@ -0,0 +1,18 @@
+package com.dealership.exceptions;
+
+@SuppressWarnings("serial")
+public class BusinessException extends Exception{
+
+	public BusinessException() {
+		super();
+		
+	}
+
+	public BusinessException(final String message) {
+		super(message);
+		
+	}
+
+	
+	
+}
