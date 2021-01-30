ed8fdb484baee25676bd10f1c7361a026a75b478
Michael Pearson <mpearson2516@yahoo.com>
2021-01-29 16:35:26 -0800

    Created the createUser method in the implementation layer

--- App/car_dealership/src/main/java/com/dealership/dao/UserDAO.java
+++ App/car_dealership/src/main/java/com/dealership/dao/UserDAO.java
@@ -1,11 +1,12 @@
 package com.dealership.dao;
 
+import com.dealership.exceptions.BusinessException;
 import com.dealership.model.User;
 
 public interface UserDAO {
 
 	// Creates a new user account
-	public int createUser(User user);
+	public int createUser(User user) throws BusinessException;
 	
 	// User logs into their account
 	public String login(String username);
--- App/car_dealership/src/main/java/com/dealership/dao/impl/UserDAOImpl.java
+++ App/car_dealership/src/main/java/com/dealership/dao/impl/UserDAOImpl.java
@@ -1,14 +1,43 @@
 package com.dealership.dao.impl;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+
 import com.dealership.dao.UserDAO;
+import com.dealership.dao.dbutil.PostgresqlConnection;
+import com.dealership.exceptions.BusinessException;
 import com.dealership.model.User;
 
 public class UserDAOImpl implements UserDAO{
 
 	@Override
-	public int createUser(User user) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int createUser(User user) throws BusinessException {
+		int c = 0;
+		try (Connection connection = PostgresqlConnection.getConnection()){
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			Date dobFormat = Date.valueOf(sdf.format(user.getDob()));
+			
+			String sql = "INSERT INTO Car_Dealership.user(username, password, name, age, dob) "
+					+ "VALUES(?, ?, ?, ?, ?)";
+			
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, user.getUsername());
+			preparedStatement.setString(2, user.getPassword());
+			preparedStatement.setString(3, user.getName());
+			preparedStatement.setInt(4, user.getAge());
+			preparedStatement.setDate(5, dobFormat);
+			
+			c = preparedStatement.executeUpdate();
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			throw new BusinessException("An Internal error has occured. Double check your input for your date of birth or try another user id. If error persists, contact the system admin.");
+		}
+
+		return c;
 	}
 
 	@Override
