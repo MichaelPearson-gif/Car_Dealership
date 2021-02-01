36a6ebbe43c5a8218f318a1d8420ee7db84dc34f
Michael Pearson <mpearson2516@yahoo.com>
2021-02-01 15:23:06 -0800

    Started creating the cars dao interface

--- App/car_dealership/src/main/java/com/dealership/dao/CarsDAO.java
+++ App/car_dealership/src/main/java/com/dealership/dao/CarsDAO.java
@@ -1,5 +1,24 @@
 package com.dealership.dao;
 
+import java.util.List;
+
+import com.dealership.exceptions.BusinessException;
+import com.dealership.model.Cars;
+
 public interface CarsDAO {
 
+	// Create a new car on the lot
+	public int newCar(Cars car) throws BusinessException;
+	
+	// Get all cars owned by a customer
+	public List<Cars> customerCars(String username) throws BusinessException;
+	
+	// Get all cars on the lot
+	public List<Cars> allCarsOnLot() throws BusinessException;
+	
+	// Update a car
+	// field represents the column in the db that is being updated
+	// value is what is being placed into the db.
+	public int carUpdate(int carId, String field, String value) throws BusinessException;
+	
 }
