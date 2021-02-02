c3f22efc8dc4e6b6e8c96f2689dc553b4663157f
Michael Pearson <mpearson2516@yahoo.com>
2021-02-02 14:24:36 -0800

    Added the service layer implementation for customer viewing their cars

--- App/car_dealership/src/main/java/com/dealership/service/CarsService.java
+++ App/car_dealership/src/main/java/com/dealership/service/CarsService.java
@@ -1,5 +1,7 @@
 package com.dealership.service;
 
+import java.util.List;
+
 import com.dealership.exceptions.BusinessException;
 import com.dealership.model.Cars;
 
@@ -7,4 +9,7 @@ public interface CarsService {
 
 	// Create a new car on the lot
 	public int newCar(Cars car) throws BusinessException;
+	
+	// Customers can view all their cars
+	public List<Cars> customerCars(String username) throws BusinessException;
 }
--- App/car_dealership/src/main/java/com/dealership/service/impl/CarsServiceImpl.java
+++ App/car_dealership/src/main/java/com/dealership/service/impl/CarsServiceImpl.java
@@ -1,5 +1,7 @@
 package com.dealership.service.impl;
 
+import java.util.List;
+
 import com.dealership.dao.CarsDAO;
 import com.dealership.dao.impl.CarsDAOImpl;
 import com.dealership.exceptions.BusinessException;
@@ -20,6 +22,18 @@ public class CarsServiceImpl implements CarsService {
 		c = carsDAO.newCar(car);
 		
 		return c;
+	}
+
+	@Override
+	public List<Cars> customerCars(String username) throws BusinessException {
+
+		// Create the list object and set it to null
+		List<Cars> myCars = null;
+		
+		// Override the list with the query results
+		myCars = carsDAO.customerCars(username);
+		
+		return myCars;
 	}
 
 }
